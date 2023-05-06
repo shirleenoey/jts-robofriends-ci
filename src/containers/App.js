0a2d4937ee2e0e56031c220f52a001aed4f56f8e
shirleen <shirleenoey@gmail.com>
2023-05-06 12:16:29 +0700

    changing files from robofriends redux to testing

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,59 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import ErrorBoundry from '../components/ErrorBoundry';
-import './App.css'
+import { setSearchField, requestRobots } from '../actions';
 
-import { setSearchField } from '../actions';
+import MainPage from '../components/MainPage'
 
-const mapStateToProps = state => {
-	return {
-		searchField: state.searchField
-	}
+import './App.css';
+
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-	return {
-		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
-	}
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
 }
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: []
-		};
-	}
+  render() {
+    return <MainPage {...this.props}/>
+  }
+}
 
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then(response=>response.json())
-			.then(users=>this.setState({robots: users}));
-	}
-
-	render() {
-		const { robots } = this.state;
-		const { searchField , onSearchChange } =  this.props;
-		const filteredRobots = robots.filter(robot=>{
-			return robot.name.toLowerCase().includes(searchField.toLowerCase())
-		})
-		return !robots.length ? 
-			<h1>Loading</h1> :
-			(
-				<div className='tc'>
-    			<h1 className='f1'>RoboFriends</h1>
-    			<SearchBox searchChange={onSearchChange}/>
-    			<Scroll>
-            <ErrorBoundry>
-    				  <CardList robots={filteredRobots}/>
-            </ErrorBoundry>
-          </Scroll>
-    		</div>
-    		);
-		}
-	}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,33 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-import './index.css';
-import App from './containers/App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { searchRobots } from './reducers';
 
-const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import { requestRobots, searchRobots } from './reducers'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store ={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+import './index.css';
+
+const logger = createLogger()
+
+const rootReducers = combineReducers({requestRobots, searchRobots})
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+registerServiceWorker();
